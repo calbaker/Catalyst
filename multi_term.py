d708bb0a7fb5c9684e60d7383d121f7ac6b1c7bf
Chad Baker <calbaker@gmail.com>
2011-06-22 14:35:08 -0500

    ???

--- multi_term.py
+++ multi_term.py
@@ -7,14 +7,14 @@ lambda_i = np.array([0.86, 3.43, 6.44, 9.53])
 terms = np.size(lambda_i)
 A_i = ( 2. * np.sin(lambda_i) / (lambda_i + np.sin(lambda_i) *
                                  np.cos(lambda_i)) ) 
-Pe = 500. 
+Pe = 50. 
 
 def get_Y(x_, y_, A_i, lambda_i, Pe):
     Y = ( sp.sum(A_i * np.exp(-4. * lambda_i**2. / Pe * x_) *
     np.cos(lambda_i * y_)) ) 
     return Y
 
-x_ = np.arange(0., 520., 10.)
+x_ = np.arange(0., 52., 2.)
 y_ = np.arange(-1., 1.001, 0.001)
 Y = np.zeros([np.size(x_), np.size(y_)])
 
@@ -23,7 +23,7 @@ for i in sp.arange(sp.size(x_)):
         Y[i,j] = get_Y(x_[i], y_[j], A_i, lambda_i, Pe)
 
 # Plot configuration
-FONTSIZE = 14
+FONTSIZE = 20
 mpl.rcParams['axes.labelsize'] = FONTSIZE
 mpl.rcParams['axes.titlesize'] = FONTSIZE
 mpl.rcParams['legend.fontsize'] = FONTSIZE
@@ -34,7 +34,7 @@ mpl.rcParams['lines.markersize'] = 10
 
 x_2d, y_2d = np.meshgrid(x_, y_)
 # TICKS = sp.arange(0,1.5,0.1)
-LEVELS = sp.arange(0, 1.2, 0.1)
+LEVELS = sp.arange(0, 1.1, 0.05)
 fig_eta = mpl.figure()
 FCS = mpl.contourf(x_2d, y_2d, Y.T, LEVELS) 
 CB = mpl.colorbar(FCS, orientation='horizontal')
@@ -42,10 +42,8 @@ mpl.grid()
 mpl.xlabel(r'$\tilde{x}$')
 mpl.ylabel(r'$\tilde{y}$')
 mpl.ylim(-1,1)
-mpl.title(r'Species Concentration v. $\tilde{x}$ and $\tilde{y}$' +
-          '\n' + str(terms) + 'terms') 
+mpl.title(r'Species Concentration v. $\tilde{x}$ and $\tilde{y}$') 
 mpl.ylim(-1, 1)
 mpl.savefig('Plots/'+ str(terms) + 'term species.pdf')
 mpl.savefig('Plots/'+ str(terms) + 'term species.png')
 
-#mpl.show()
