a00d341c582843e0077147aa97d389adfaecc875
Chad Baker <calbaker@gmail.com>
2011-06-23 21:59:15 -0500

    superficial changes

--- eigen.py
+++ eigen.py
@@ -3,7 +3,7 @@ import scipy as sp
 import matplotlib.pyplot as mpl
 
 lambda_guess = np.arange(0, 0.1, 0.0001)
-Da = 0.002
+Da = 0.003
 
 def get_solution(lambda_guess, Da):
     solution = 1 - lambda_guess / Da * np.tan(lambda_guess)
