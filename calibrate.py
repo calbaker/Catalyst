e5aa59ea44fb0f4ae51c4d037208e64cb5ba1c61
Chad Baker <calbaker@gmail.com>
2011-06-26 17:18:47 -0500

    I scrapped a lot of what I was doing to use the Gauss-Newton method from Chopra.

--- calibrate.py
+++ calibrate.py
@@ -26,33 +26,3 @@ def get_R(real_cat, data1):
     R = sp.sum((real_cat.eta_ij[0,:] - data1.eta_T)**2)
     return R
 
-R = sp.empty(0)
-A_arr = sp.array([1., 2., 3.]) * 1.e10
-T_a = sp.array([14., 15., 16.]) * 1.e3
-
-real_cat.T_a = T_a[0]
-
-for j in sp.arange(3):
-    real_cat.A_arr = A_arr[j]
-    R = sp.append(R, get_R(real_cat, data1))
-dRdA_arr = (R[1:] - R[:-1]) / (A_arr[1:] - A_arr[:-1])
-d2RdA_arr2 = sp.array([(dRdA_arr[1] - dRdA_arr[0]) / (A_arr[1] - A_arr[0])])
-
-# for k in sp.arange(3):
-#     real_cat.T_a = T_a[k]
-#     R = sp.append(R, get_R(real_cat, data1))
-# dRdT_a = (R[4:] - R[3:-1]) / (T_a[1:] - T_a[:-1])
-# d2RdT_a2 = sp.array([(dRdT_a[1] - dRdT_a[0]) / (T_a[1] - T_a[0])])
-
-i = 2
-j = 2
-
-A_arr = sp.append(A_arr, -dRdA_arr[j-1] / d2RdA_arr2[j-2])
-real_cat.A_arr = A_arr[j+1]
-R = sp.append(R, get_R(real_cat, data1))
-dRdA_arr = sp.append(dRdA_arr, (R[i+1] - R[i]) / (A_arr[j+1] -
-                                                  A_arr[j]))   
-d2RdA_arr2 = sp.append(d2RdA_arr2, (dRdA_arr[j] - dRdA_arr[j-1]) /
-                       (A_arr[j+1] - A_arr[j]))  
-i = i + 1
-j = j + 1
