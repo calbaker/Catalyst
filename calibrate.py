0a49318b58b47ac4c435f1e8afeecb116296a53f
Chad Baker <calbaker@gmail.com>
2011-07-06 10:40:26 -0500

    nothing substantial

--- calibrate.py
+++ calibrate.py
@@ -11,7 +11,7 @@ reload(expdata)
 
 data1 = expdata.Data()
 data1.source = 'Osman July 3'
-data1.Vdot = sp.array([250.]) * 1.e-6 / 60. 
+data1.Vdot = 250. * 1.e-6 / 60. 
 data1.T_array = sp.arange(350., 625., 25.)
 data1.HCin = sp.array([[4900., 4950., 4940.], [4900., 4920., 4950.],
                        [5150., 5170., 5140.], [5760., 5450., 5560.],
@@ -26,6 +26,7 @@ data1.HCout = sp.array([[3910., 3895., 3890.], [3730., 3740., 3720.],
                         [0239., 0220., 0225.], [0111., 0098., 0103.],
                         [0053., 0041., 0045.]])
 
+data1.Vdot_array = sp.array([data1.Vdot])
 data1.set_params()
 
 # Plot configuration
@@ -41,8 +42,9 @@ plt.rcParams['lines.markersize'] = 5
 fig = plt.figure()
 plt.plot(data1.T_array, data1.eta_mean, linestyle='', marker='x',
          markersize=15) 
-plt.plot(data1.T_array, data1.eta_dim.T, linestyle='', marker='.',
-         markersize=15) 
+data1.T_array = sp.arange(350., 605., 5.)
+data1.set_eta_dim()
+plt.plot(data1.T_array, data1.eta_dim.T, linestyle='-') 
 
 plt.grid()
 plt.xlabel('Temperature (C)')
