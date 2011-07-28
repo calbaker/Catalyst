bd81a5aaad84ff83211d54abe75fdcee4add15e9
Chad Baker <calbaker@gmail.com>
2011-07-27 20:55:58 -0500

    Messed around with data source.

--- calibrate.py
+++ calibrate.py
@@ -10,9 +10,9 @@ os.chdir('/home/chad/Documents/UT Stuff/Research/Catalyst/Model')
 import experimental_data as expdata
 reload(expdata)
 
-data1 = expdata.Data()
+cal_data = expdata.Data()
 
-data1.Vdot = 250. * 1.e-6 / 60.
+cal_data.Vdot = 250. * 1.e-6 / 60.
 source = '250sccm 10nm PtPd variedT.xls'
 # Define the path to the .xls file(s) containing the conversion data.
 # import the worksheet as a sheet object
@@ -20,29 +20,29 @@ data_worksheet = xlrd.open_workbook(filename=source).sheet_by_index(0)
     
 # Import conversion data from worksheet and store as scipy arrays
 
-data1.T_raw = sp.array(data_worksheet.col_values(0, start_rowx=4, 
+cal_data.T_raw = sp.array(data_worksheet.col_values(0, start_rowx=4, 
                                                  end_rowx=None)) 
-data1.T_exp = data1.T_raw[0::3]
-data1.HCout_raw = sp.array(data_worksheet.col_values(4, start_rowx=4, 
+cal_data.T_exp = cal_data.T_raw[0::3]
+cal_data.HCout_raw = sp.array(data_worksheet.col_values(4, start_rowx=4, 
                                                      end_rowx=None))
-data1.HCout = sp.array([data1.HCout_raw[0::3],
-                        data1.HCout_raw[1::3],
-                        data1.HCout_raw[2::3]]).T
-data1.HCin_raw = sp.array(data_worksheet.col_values(8, start_rowx=4,
+cal_data.HCout = sp.array([cal_data.HCout_raw[0::3],
+                        cal_data.HCout_raw[1::3],
+                        cal_data.HCout_raw[2::3]]).T
+cal_data.HCin_raw = sp.array(data_worksheet.col_values(8, start_rowx=4,
                                                     end_rowx=None))
-data1.HCin = sp.array([data1.HCin_raw[0::3],
-                        data1.HCin_raw[1::3],
-                        data1.HCin_raw[2::3]]).T
-data1.HCin = data1.HCin[1:-2]
-data1.HCout = data1.HCout[1:-2]
-data1.T_exp = data1.T_exp[1:-2]
+cal_data.HCin = sp.array([cal_data.HCin_raw[0::3],
+                        cal_data.HCin_raw[1::3],
+                        cal_data.HCin_raw[2::3]]).T
+cal_data.HCin = cal_data.HCin[1:-2]
+cal_data.HCout = cal_data.HCout[1:-2]
+cal_data.T_exp = cal_data.T_exp[1:-2]
    
-data1.Vdot_array = sp.array([data1.Vdot])
-data1.set_eta()
-data1.p0 = sp.array([5.e10, 12.e3])
-data1.set_params()
-data1.T_array = sp.arange(375., 555., 5.)
-data1.set_eta_dim()
+cal_data.Vdot_array = sp.array([cal_data.Vdot])
+cal_data.set_eta()
+cal_data.p0 = sp.array([5.e12, 13.e3])
+cal_data.set_params()
+cal_data.T_array = sp.arange(325., 655., 5.)
+cal_data.set_eta_dim()
 
 
 # Plot configuration
@@ -56,8 +56,8 @@ plt.rcParams['lines.linewidth'] = 1.5
 plt.rcParams['lines.markersize'] = 8
 
 fig = plt.figure()
-plt.plot(data1.T_exp, data1.eta_mean, ' sk', label='Experiment')  
-plt.plot(data1.T_array, data1.eta_dim.T, '-b', label='Model')
+plt.plot(cal_data.T_exp, cal_data.eta_mean, ' sk', label='Experiment')  
+plt.plot(cal_data.T_array, cal_data.eta_dim.T, '-b', label='Model')
 plt.xlim(350, 575)
 plt.legend(loc='best')
 plt.grid()
