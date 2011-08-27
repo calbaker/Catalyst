87a41ae413ca57bd44aa6e002bfab975f12de785
Chad Baker <calbaker@gmail.com>
2011-08-27 15:39:56 -0500

    plot configuration

--- eigen.py
+++ eigen.py
@@ -12,7 +12,7 @@ def get_solution(lambda_guess, Da):
 solution = get_solution(lambda_guess, Da)
 
 # Plot configuration
-FONTSIZE = 20
+FONTSIZE = 30
 plt.rcParams['axes.labelsize'] = FONTSIZE
 plt.rcParams['axes.titlesize'] = FONTSIZE
 plt.rcParams['legend.fontsize'] = FONTSIZE
@@ -22,12 +22,14 @@ plt.rcParams['lines.linewidth'] = 1.5
 plt.rcParams['lines.markersize'] = 10
 
 fig1 = plt.figure()
-fig1.subplots_adjust(bottom=0.12)
+fig1.subplots_adjust(bottom=0.15)
+fig1.subplots_adjust(left=0.2)
 plt.plot(lambda_guess, solution)
 #plt.xlim(0, 2)
 plt.ylim(-10, 10)
 plt.grid()
 plt.xlabel(r'$\lambda$')
+plt.ylabel(r'$1 - \frac{\lambda}{Da} \lambda$') 
 #plt.title('Eigenvalues\nDa='+str(Da))
 plt.savefig('Plots/eigen.pdf')
 plt.savefig('Plots/eigen.png')
--- multi_term.py
+++ multi_term.py
@@ -1,6 +1,6 @@
 import numpy as np
 import scipy as sp
-import matplotlib.pyplot as mpl
+import matplotlib.pyplot as plt
 
 # For Da = 1,
 lambda_i = np.array([0.86, 3.43, 6.44, 9.53])
@@ -14,8 +14,8 @@ def get_Y(x_, y_, A_i, lambda_i, Pe):
     np.cos(lambda_i * y_)) ) 
     return Y
 
-x_ = np.arange(0., 52., 2.)
-y_ = np.arange(-1., 1.001, 0.001)
+x_ = np.linspace(0., 20., 50)
+y_ = np.linspace(-1., 1., 100)
 Y = np.zeros([np.size(x_), np.size(y_)])
 
 for i in sp.arange(sp.size(x_)):
@@ -23,27 +23,31 @@ for i in sp.arange(sp.size(x_)):
         Y[i,j] = get_Y(x_[i], y_[j], A_i, lambda_i, Pe)
 
 # Plot configuration
-FONTSIZE = 20
-mpl.rcParams['axes.labelsize'] = FONTSIZE
-mpl.rcParams['axes.titlesize'] = FONTSIZE
-mpl.rcParams['legend.fontsize'] = FONTSIZE
-mpl.rcParams['xtick.labelsize'] = FONTSIZE
-mpl.rcParams['ytick.labelsize'] = FONTSIZE
-mpl.rcParams['lines.linewidth'] = 1.5
-mpl.rcParams['lines.markersize'] = 10
+FONTSIZE = 30
+plt.rcParams['axes.labelsize'] = FONTSIZE
+plt.rcParams['axes.titlesize'] = FONTSIZE
+plt.rcParams['legend.fontsize'] = FONTSIZE
+plt.rcParams['xtick.labelsize'] = FONTSIZE
+plt.rcParams['ytick.labelsize'] = FONTSIZE
+plt.rcParams['lines.linewidth'] = 1.5
+plt.rcParams['lines.markersize'] = 10
 
 x_2d, y_2d = np.meshgrid(x_, y_)
-# TICKS = sp.arange(0,1.5,0.1)
+TICKS = sp.arange(0,1.2,0.2)
 LEVELS = sp.arange(0, 1.1, 0.05)
-fig_eta = mpl.figure()
-FCS = mpl.contourf(x_2d, y_2d, Y.T, LEVELS) 
-CB = mpl.colorbar(FCS, orientation='horizontal')
-mpl.grid()
-mpl.xlabel(r'$\tilde{x}$')
-mpl.ylabel(r'$\tilde{y}$')
-mpl.ylim(-1,1)
-mpl.title(r'Species Concentration v. $\tilde{x}$ and $\tilde{y}$') 
-mpl.ylim(-1, 1)
-mpl.savefig('Plots/'+ str(terms) + 'term species.pdf')
-mpl.savefig('Plots/'+ str(terms) + 'term species.png')
+fig_eta = plt.figure()
+FCS = plt.contourf(x_2d, y_2d, Y.T, levels=LEVELS)  
+CB = plt.colorbar(FCS, orientation='vertical', ticks=TICKS)
+plt.grid()
+plt.xlabel(r'$\tilde{x}$')
+plt.ylabel(r'$\tilde{y}$')
+plt.ylim(-1,1)
+#plt.title(r'Species Concentration v. $\tilde{x}$ and $\tilde{y}$') 
+plt.ylim(-1, 1)
+plt.subplots_adjust(bottom=0.15)
+plt.subplots_adjust(left=0.2)
+plt.subplots_adjust(right=0.7)
+plt.savefig('Plots/'+ str(terms) + 'term species.pdf')
+plt.savefig('Plots/'+ str(terms) + 'term species.png')
 
+plt.show()
