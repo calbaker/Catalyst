fc7d401680b9902d35324ffb8ad2e269bc544d1e
Chad Baker <calbaker@gmail.com>
2011-08-27 16:55:00 -0500

    changed plot

--- multi_term.py
+++ multi_term.py
@@ -2,6 +2,8 @@ import numpy as np
 import scipy as sp
 import matplotlib.pyplot as plt
 
+import first_term as ft
+
 # For Da = 1,
 lambda_i = np.array([0.86, 3.43, 6.44, 9.53])
 terms = np.size(lambda_i)
@@ -14,7 +16,7 @@ def get_Y(x_, y_, A_i, lambda_i, Pe):
     np.cos(lambda_i * y_)) ) 
     return Y
 
-x_ = np.linspace(0., 20., 50)
+x_ = np.linspace(0., 200., 50)
 y_ = np.linspace(-1., 1., 100)
 Y = np.zeros([np.size(x_), np.size(y_)])
 
@@ -49,5 +51,32 @@ plt.subplots_adjust(left=0.2)
 plt.subplots_adjust(right=0.7)
 plt.savefig('Plots/'+ str(terms) + 'term species.pdf')
 plt.savefig('Plots/'+ str(terms) + 'term species.png')
+
+cat1 = ft.One_Term_Catalyst()
+
+cat1.x_array = np.linspace(0., 200., 50)
+cat1.y_array = np.linspace(-1., 1, 100)
+cat1.Pe = 50.
+cat1.set_Yxy_()
+
+fig_species = plt.figure()
+x_2d, y_2d = np.meshgrid(cat1.x_array, cat1.y_array)
+# TICKS = sp.arange(0,1.5,0.1)
+LEVELS = sp.arange(0, 1.2, 0.1)
+FCS = plt.contourf(x_2d, y_2d, cat1.Yxy_.T) 
+CB = plt.colorbar(FCS, orientation='vertical', format='%.2f')
+plt.grid()
+plt.xlabel(r'$\tilde{x}$')
+plt.ylabel(r'$\tilde{y}$')
+# plt.title(r'Species Concentration v. $\tilde{x}$ and $\tilde{y}$' +
+#           '\nDa=' + str(cat1.Da) + ' Pe=' + str(cat1.Pe))  
+plt.ylim(-1, 1)
+plt.subplots_adjust(bottom=0.15)
+plt.subplots_adjust(left=0.2)
+plt.subplots_adjust(right=0.7)
+plt.savefig('Plots/species Da=' + str(cat1.Da) + ' Pe=' + str(cat1.Pe)
+            + '.pdf') 
+plt.savefig('Plots/species Da=' + str(cat1.Da) + ' Pe=' + str(cat1.Pe)
+            + '.png')  
 
 plt.show()
