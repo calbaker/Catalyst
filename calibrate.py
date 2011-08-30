acd4a22975ff9b8b6937c6a82d837e99718b4977
Chad Baker <calbaker@gmail.com>
2011-08-30 11:35:15 -0500

    added 500sccm source file

--- calibrate.py
+++ calibrate.py
@@ -34,6 +34,27 @@ data250.p0 = sp.array([1e7, 5e3])
 data250.set_params()
 data250.set_eta_dim()
 
+data500 = expdata.Data()
+data500.T_a = data250.T_a
+data500.A_arr = data250.A_arr
+data500.Vdot = 500. * 1.e-6 / 60.
+data500.source = '500sccm 10nmPtPd VariedT aug.xls'
+# Define the path to the .xls file(s) containing the conversion data.
+# import the worksheet as a sheet object
+data500.worksheet = xlrd.open_workbook(filename=data500.source).sheet_by_index(0)
+# Import conversion data from worksheet and store as scipy arrays
+data500.T_raw = sp.array(data500.worksheet.col_values(0, start_rowx=4, 
+                                                 end_rowx=None)) 
+data500.T_exp = data500.T_raw
+data500.HCout_raw = sp.array(data500.worksheet.col_values(4, start_rowx=4, 
+                                                     end_rowx=None))
+data500.HCin_raw = sp.array(data500.worksheet.col_values(8, start_rowx=4,
+                                                    end_rowx=None))
+data500.eta_mean = (data500.HCin_raw - data500.HCout_raw) / data500.HCin_raw
+data500.T_array = sp.linspace(250., 650., 100)
+data500.Vdot_array = sp.array([data500.Vdot])
+data500.set_eta_dim()
+
 data750 = expdata.Data()
 data750.T_a = data250.T_a
 data750.A_arr = data250.A_arr
@@ -71,6 +92,11 @@ plt.plot(data250.T_exp, data250.eta_mean * 100., 'sr', linestyle='',
          label='250sccm exp')
 plt.plot(data250.T_array, data250.eta_dim.T * 100., '-r',
          label='250sccm model')
+
+plt.plot(data500.T_exp, data500.eta_mean * 100., 'sk', linestyle='',
+         label='500sccm exp')
+plt.plot(data500.T_array, data500.eta_dim.T * 100., '-k',
+         label='500sccm model')
 
 plt.plot(data750.T_exp, data750.eta_mean * 100., 'sb', linestyle='',
          label='750sccm exp')
