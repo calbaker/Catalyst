ca72ea645c61f42d57adb05d4d6020872ca5b873
Chad Baker <calbaker@gmail.com>
2011-08-31 19:05:02 -0500

    double yep

--- plot_multi_term.py
+++ plot_multi_term.py
@@ -9,7 +9,7 @@ os.chdir('/home/chad/Documents/UT Stuff/Research/Catalyst/Model')
 import multi_term 
 reload(multi_term)
 
-cat1 = mt.One_Term_Catalyst()
+cat1 = multi_term.Catalyst()
 
 cat1.A_arr = 5576796142069.6602
 cat1.T_a = 15325.877039480061
@@ -44,24 +44,24 @@ lambda_1_curve = cat1.get_lambda(Da_range)
 # plt.savefig('Plots/eigen_fit.pdf')
 # plt.savefig('Plots/eigen_fit.png')
 
-fig_species = plt.figure()
-x_2d, y_2d = np.meshgrid(cat1.x_array, cat1.y_array)
-# TICKS = sp.arange(0,1.5,0.1)
-LEVELS = sp.arange(0, 1.2, 0.1)
-FCS = plt.contourf(x_2d, y_2d, cat1.Yxy_.T) 
-CB = plt.colorbar(FCS, orientation='vertical', format='%.2f')
-plt.grid()
-plt.xlabel(r'$\tilde{x}$')
-plt.ylabel(r'$\tilde{y}$')
-# plt.title(r'Species Concentration v. $\tilde{x}$ and $\tilde{y}$' +
-#           '\nDa=' + str(cat1.Da) + ' Pe=' + str(cat1.Pe))  
-plt.ylim(-1, 1)
-plt.subplots_adjust(bottom=0.15)
-plt.subplots_adjust(left=0.15)
-plt.savefig('Plots/species Da=' + str(cat1.Da) + ' Pe=' + str(cat1.Pe)
-            + '.pdf') 
-plt.savefig('Plots/species Da=' + str(cat1.Da) + ' Pe=' + str(cat1.Pe)
-            + '.png')  
+# fig_species = plt.figure()
+# x_2d, y_2d = np.meshgrid(cat1.x_array, cat1.y_array)
+# # TICKS = sp.arange(0,1.5,0.1)
+# LEVELS = sp.arange(0, 1.2, 0.1)
+# FCS = plt.contourf(x_2d, y_2d, cat1.Yxy_.T) 
+# CB = plt.colorbar(FCS, orientation='vertical', format='%.2f')
+# plt.grid()
+# plt.xlabel(r'$\tilde{x}$')
+# plt.ylabel(r'$\tilde{y}$')
+# # plt.title(r'Species Concentration v. $\tilde{x}$ and $\tilde{y}$' +
+# #           '\nDa=' + str(cat1.Da) + ' Pe=' + str(cat1.Pe))  
+# plt.ylim(-1, 1)
+# plt.subplots_adjust(bottom=0.15)
+# plt.subplots_adjust(left=0.15)
+# plt.savefig('Plots/species Da=' + str(cat1.Da) + ' Pe=' + str(cat1.Pe)
+#             + '.pdf') 
+# plt.savefig('Plots/species Da=' + str(cat1.Da) + ' Pe=' + str(cat1.Pe)
+#             + '.png')  
 
 FONTSIZE = 15
 plt.rcParams['axes.labelsize'] = FONTSIZE
@@ -73,10 +73,11 @@ plt.rcParams['lines.linewidth'] = 1.5
 plt.rcParams['lines.markersize'] = 10
 
 fig_eta = plt.figure()
-Pe_2d, Da_2d = np.meshgrid(cat1.Pe_array, cat1.Da_array)
-# TICKS = sp.arange(0,1.5,0.1)
+Pe_2d = cat1.Pe_array.T
+dummy, Da_2d = np.meshgrid(cat1.Pe_array[:,0], cat1.Da_array)
+TICKS = sp.arange(0,1.5,0.1)
 LEVELS = sp.arange(0, 1.05, 0.05)
-FCS = plt.contourf(Pe_2d, Da_2d, cat1.eta_dimless.T, LEVELS) 
+FCS = plt.contourf(Pe_2d, Da_2d, cat1.eta.T) 
 CB = plt.colorbar(FCS, orientation='vertical', format='%.2f')
 plt.grid()
 plt.xlabel('Pe')
@@ -84,25 +85,6 @@ plt.ylabel('Da')
 plt.title('Species Conversion Efficiency')
 plt.savefig('Plots/eta.pdf')
 plt.savefig('Plots/eta.png')
-
-cat1.A_arr = 25950754583570564.0
-cat1.T_a = 22056.187248619895
-cat1.set_eta_dim()
-Vdot2d, T2d = np.meshgrid(cat1.Vdot_array, cat1.T_array)
-Vdot2d = Vdot2d * 60. * 1.e6
-# converts back to sccm for plotting
-# TICKS = sp.arange(0,1.5,0.1)
-LEVELS = sp.arange(0., 1.05, 0.05)
-fig_eta_real = plt.figure()
-FCS = plt.contourf(Vdot2d, T2d, cat1.eta_dim.T, LEVELS)
-CB = plt.colorbar(FCS, orientation='vertical')
-plt.grid()
-plt.xlabel(r'$\dot{V}$ (sccm)')
-plt.ylabel('T (K)')
-plt.title(
-    'Conversion Efficiency v.\nFlow Rate and Temperature') 
-plt.savefig('Plots/real eta.pdf')
-plt.savefig('Plots/real eta.png')
 
 plt.show()
 
