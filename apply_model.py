eaf848cf6baaee4a7fa0b9b3300c2e96d2646196
Chad Baker <calbaker@gmail.com>
2011-12-07 14:22:31 -0600

    works!

--- apply_model.py
+++ apply_model.py
@@ -14,14 +14,17 @@ cat = multi_term.Catalyst()
 cat.T_a = 7207.
 cat.A_arr = 1.005e7
 
+cat.Vdot = 500.e-6 / 60. 
+cat.T = 450.
 
-cat.thickness_array = np.linspace(1,100,50)
-cat.eta_t = np.zeros(cat.thickness.size)
+cat.thickness_array = np.linspace(1,150,50) * 1.e-6
+cat.eta_t = np.zeros(cat.thickness_array.size)
 
-for i in range(cat.eta_t.shape[0])):
-    cat.thickness cat.thickness_array[i]
-    cat.set_eta()
-    cat.eta_t[i] = cat.eta 
+for i in range(cat.eta_t.shape[0]):
+    cat.thickness = cat.thickness_array[i]
+    cat.Pe = cat.get_Pe(cat.Vdot, cat.T)
+    cat.Da = cat.get_Da(cat.T, cat.A_arr, cat.T_a)
+    cat.eta_t[i] = cat.get_eta(cat.Pe, cat.Da)
 
 # Plot configuration
 FONTSIZE = 18
@@ -33,7 +36,7 @@ plt.rcParams['ytick.labelsize'] = FONTSIZE
 plt.rcParams['lines.linewidth'] = 1.5
 plt.rcParams['lines.markersize'] = 10
 
-plt.plot(cat.thickness * 1.e6, cat.eta_t)
+plt.plot(cat.thickness_array * 1.e6, cat.eta_t)
 plt.xlabel(r'Nanowire Height ($\mu$m)')
 plt.ylabel('Conversion Efficiency')
 plt.grid()
