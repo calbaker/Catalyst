27aba47b0a7843e7271ed337e299b8ea4e3024a3
Chad Baker <calbaker@gmail.com>
2011-12-07 14:16:14 -0600

    apply_model should be working now

--- apply_model.py
+++ apply_model.py
@@ -14,7 +14,28 @@ cat = multi_term.Catalyst()
 cat.T_a = 7207.
 cat.A_arr = 1.005e7
 
-cat.eta_t = np.zeros(50)
 
-# for i in range(cat.eta_t.shape[0]):
-    
+cat.thickness_array = np.linspace(1,100,50)
+cat.eta_t = np.zeros(cat.thickness.size)
+
+for i in range(cat.eta_t.shape[0])):
+    cat.thickness cat.thickness_array[i]
+    cat.set_eta()
+    cat.eta_t[i] = cat.eta 
+
+# Plot configuration
+FONTSIZE = 18
+plt.rcParams['axes.labelsize'] = FONTSIZE
+plt.rcParams['axes.titlesize'] = FONTSIZE
+plt.rcParams['legend.fontsize'] = FONTSIZE - 5
+plt.rcParams['xtick.labelsize'] = FONTSIZE
+plt.rcParams['ytick.labelsize'] = FONTSIZE
+plt.rcParams['lines.linewidth'] = 1.5
+plt.rcParams['lines.markersize'] = 10
+
+plt.plot(cat.thickness * 1.e6, cat.eta_t)
+plt.xlabel(r'Nanowire Height ($\mu$m)')
+plt.ylabel('Conversion Efficiency')
+plt.grid()
+plt.savefig('Plots/eta_applied.pdf')
+plt.savefig('Plots/eta_applied.png')
