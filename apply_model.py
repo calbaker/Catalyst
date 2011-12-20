8c9f27e04d3229aaa5d18b777be2c087d2a58ecb
Chad Baker <calbaker@gmail.com>
2011-12-20 10:58:17 -0600

    should be ready for the paper

--- apply_model.py
+++ apply_model.py
@@ -25,18 +25,21 @@ cat.A_arr_array = ( cat.A_arr0 / (cat.thickness_array /
                                   cat.thickness0) )
 cat.Kn_length_array = np.array([1., 5., 10., 50., 100.]) * 1.e-9
 
-cat.eta2d = np.zeros([cat.thickness_array.size, cat.A_arr_array.size])
+cat.eta_Pt_density = np.zeros([cat.thickness_array.size, cat.A_arr_array.size])
+cat.eta_Pt_total = np.zeros([cat.thickness_array.size, cat.A_arr_array.size])
 
-for i in range(cat.thickness_array.size):
-    cat.thickness = cat.thickness_array[i]
-#    cat.A_arr = cat.A_arr_array[i] ??? !!! consider presenting plots
-#    based on this line also
-    cat.A_arr = cat.A_arr0
-    for j in range(cat.Kn_length_array.size):
-        cat.Kn_length = cat.Kn_length_array[j]
-        cat.Pe = cat.get_Pe(cat.Vdot, cat.T)
+cat.Pe = cat.get_Pe(cat.Vdot, cat.T)
+
+for j in range(cat.Kn_length_array.size):
+    cat.Kn_length = cat.Kn_length_array[j]
+    for i in range(cat.thickness_array.size):
+        cat.thickness = cat.thickness_array[i]
+        cat.A_arr = cat.A_arr_array[i]
         cat.Da = cat.get_Da(cat.T, cat.A_arr, cat.T_a)
-        cat.eta2d[i,j] = cat.get_eta(cat.Pe, cat.Da)
+        cat.eta_Pt_total[i,j] = cat.get_eta(cat.Pe, cat.Da)
+        cat.A_arr = cat.A_arr0
+        cat.Da = cat.get_Da(cat.T, cat.A_arr, cat.T_a)
+        cat.eta_Pt_density[i,j] = cat.get_eta(cat.Pe, cat.Da)
 
 
 # Plot configuration
@@ -46,24 +49,37 @@ plt.rcParams['axes.titlesize'] = FONTSIZE
 plt.rcParams['legend.fontsize'] = FONTSIZE - 5
 plt.rcParams['xtick.labelsize'] = FONTSIZE
 plt.rcParams['ytick.labelsize'] = FONTSIZE
-plt.rcParams['lines.linewidth'] = 1.5
+plt.rcParams['lines.linewidth'] = 2.
 plt.rcParams['lines.markersize'] = 10
 
 
 FORMATTER = tic.FormatStrFormatter('%1.1f')
-MARKERS = ['-k', '--b', '-.r', ':y', '-g']
+MARKERS = ['-k', '--b', '-.r', ':m', '-g']
 LABELS = list(['1 nm', '5 nm', '10 nm', '50 nm', '100 nm'])
 
 plt.figure()
 for i in range(cat.Kn_length_array.size):
-    plt.plot(cat.thickness_array * 1.e6, cat.eta2d[:,i] * 100., MARKERS[i])
-#             , label=LABELS[i])
+    plt.plot(cat.thickness_array * 1.e6, cat.eta_Pt_density[:,i] *
+             100., MARKERS[i], label=LABELS[i]) 
 plt.xlabel(r'Nanowire Height ($\mu$m)')
 plt.ylabel('Conversion Efficiency (%)')
 #ax1.yaxis.set_major_formatter(FORMATTER)
 plt.grid()
-plt.savefig('Plots/eta_applied.pdf')
-plt.savefig('Plots/eta_applied.png')
+plt.savefig('Plots/applied/eta_Pt_density.pdf')
+plt.savefig('Plots/applied/eta_Pt_density.png')
+plt.legend(loc='lower left')
+plt.subplots_adjust(left=0.15)
+
+plt.figure()
+for i in range(cat.Kn_length_array.size):
+    plt.plot(cat.thickness_array * 1.e6, cat.eta_Pt_total[:,i] *
+             100., MARKERS[i], label=LABELS[i]) 
+plt.xlabel(r'Nanowire Height ($\mu$m)')
+plt.ylabel('Conversion Efficiency (%)')
+#ax1.yaxis.set_major_formatter(FORMATTER)
+plt.grid()
+plt.savefig('Plots/applied/eta_Pt_total.pdf')
+plt.savefig('Plots/applied/eta_Pt_total.png')
 plt.legend(loc='lower left')
 plt.subplots_adjust(left=0.15)
 
