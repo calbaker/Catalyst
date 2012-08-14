4820043870c665956864505f9c4e50c12532a2c5
Chad Baker <calbaker@utexas.edu>
2012-08-14 11:01:00 -0500

    runs, but Y is wacky high

--- Instances/plot_species.py
+++ Instances/plot_species.py
@@ -2,35 +2,45 @@
 dimensions, one for first term and one for multi term."""
 
 import numpy as np
-
 import matplotlib.pyplot as plt
 import os
+import sys
 
-import first_term
-reload(first_term)
-import multi_term 
-reload(multi_term)
+cmd_folder = os.path.dirname('../Modules/')
+if cmd_folder not in sys.path:
+    sys.path.insert(0, cmd_folder)
+
+import catalyst
+reload(catalyst)
 
 x_array = np.linspace(0., 100., 100)
-y_array = np.linspace(-1., 1., 100)
+y_array = np.linspace(0, 1., 50)
 
-Pe = 50.
-Da = 1.
+Pe = 25.
+Da = 0.1
+# reasonable Pe and Da for experimental conditions
 
-cat4 = multi_term.Catalyst()
+cat4 = catalyst.Catalyst()
 cat4.x_array = x_array
 cat4.y_array = y_array
 cat4.Pe = Pe
 cat4.Da = Da
-cat4.set_Yxy(Pe,Da)
+cat4.Yxy = np.zeros([x_array.size, y_array.size])
 
-cat1 = first_term.One_Term_Catalyst()
+cat1 = catalyst.Catalyst(terms=1)
 cat1.x_array = x_array
 cat1.y_array = y_array
 cat1.Pe = Pe
 cat1.Da = Da
-cat1.set_Yxy(Pe,Da)
+cat1.Yxy = np.zeros([x_array.size, y_array.size])
 
+for i in range(x_array.size):
+    x_ = x_array[i]
+    for j in range(y_array.size):
+        y_ = y_array[j]
+
+        cat1.Yxy[i, j] = cat1.get_Y(x_, y_, Pe=Pe, Da=Da)
+        cat4.Yxy[i, j] = cat4.get_Y(x_, y_, Pe=Pe, Da=Da)
 
 # Plot configuration
 FONTSIZE = 30
@@ -42,44 +52,44 @@ plt.rcParams['ytick.labelsize'] = FONTSIZE
 plt.rcParams['lines.linewidth'] = 1.5
 plt.rcParams['lines.markersize'] = 10
 
-TICKS = np.arange(0,1.2,0.2)
+TICKS = np.arange(0, 1.2, 0.2)
 LEVELS = np.arange(0, 1.1, 0.1)
 
 x_2d, y_2d = np.meshgrid(x_array, y_array)
 fig_eta = plt.figure()
-FCS = plt.contourf(x_2d, y_2d, cat4.Yxy.T, levels=LEVELS)
-CB = plt.colorbar(FCS, orientation='vertical', ticks=TICKS)
+FCS = plt.contourf(x_2d, y_2d, cat4.Yxy.T)#, levels=LEVELS)
+CB = plt.colorbar(FCS, orientation='vertical')#, ticks=TICKS)
 plt.grid()
 plt.xlabel(r'$\tilde{x}$')
 plt.ylabel(r'$\tilde{y}$')
-plt.ylim(-1,1)
-#plt.title(r'Species Concentration v. $\tilde{x}$ and $\tilde{y}$') 
+plt.ylim(-1, 1)
+#plt.title(r'Species Concentration v. $\tilde{x}$ and $\tilde{y}$')
 plt.ylim(-1, 1)
 plt.subplots_adjust(bottom=0.15)
 plt.subplots_adjust(left=0.2)
 plt.subplots_adjust(right=0.7)
-plt.savefig('Plots/4species Da=' + str(cat1.Da) + ' Pe=' + str(cat1.Pe)
-            + '.pdf') 
-plt.savefig('Plots/4species Da=' + str(cat1.Da) + ' Pe=' + str(cat1.Pe)
-            + '.png')  
+plt.savefig('../Plots/4species Da=' + str(cat1.Da) + ' Pe=' + str(cat1.Pe)
+            + '.pdf')
+plt.savefig('../Plots/4species Da=' + str(cat1.Da) + ' Pe=' + str(cat1.Pe)
+            + '.png')
 
 fig_species = plt.figure()
 # TICKS = np.arange(0,1.5,0.1)
 LEVELS = np.arange(0, 1.2, 0.1)
-FCS = plt.contourf(x_2d, y_2d, cat1.Yxy.T, levels=LEVELS) 
+FCS = plt.contourf(x_2d, y_2d, cat1.Yxy.T, levels=LEVELS)
 CB = plt.colorbar(FCS, orientation='vertical', format='%.2f', ticks=TICKS)
 plt.grid()
 plt.xlabel(r'$\tilde{x}$')
 plt.ylabel(r'$\tilde{y}$')
 # plt.title(r'Species Concentration v. $\tilde{x}$ and $\tilde{y}$' +
-#           '\nDa=' + str(cat1.Da) + ' Pe=' + str(cat1.Pe))  
+#           '\nDa=' + str(cat1.Da) + ' Pe=' + str(cat1.Pe))
 plt.ylim(-1, 1)
 plt.subplots_adjust(bottom=0.15)
 plt.subplots_adjust(left=0.2)
 plt.subplots_adjust(right=0.7)
-plt.savefig('Plots/species Da=' + str(cat1.Da) + ' Pe=' + str(cat1.Pe)
-            + '.pdf') 
-plt.savefig('Plots/species Da=' + str(cat1.Da) + ' Pe=' + str(cat1.Pe)
-            + '.png')  
+plt.savefig('../Plots/species Da=' + str(cat1.Da) + ' Pe=' + str(cat1.Pe)
+            + '.pdf')
+plt.savefig('../Plots/species Da=' + str(cat1.Da) + ' Pe=' + str(cat1.Pe)
+            + '.png')
 
 plt.show()
