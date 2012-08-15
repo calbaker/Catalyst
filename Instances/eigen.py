73ee9a8937a6a1a6b21b83555a6ce74fed1c734e
Chad Baker <calbaker@utexas.edu>
2012-08-15 12:27:31 -0500

    much more convenient to run in command line with arguments

--- Instances/eigen.py
+++ Instances/eigen.py
@@ -13,57 +13,57 @@ reload(catalyst)
 
 cat_eigen = catalyst.Catalyst()
 
-Da = 0.5
+def run_eigen(Da, MIN, MAX):
+    """Does everything important in eigen.py
 
-guess = np.arange(0, 30., 0.01)
+    Inputs:
+    Da : Dahmkohler number
+    MIN, MAX : limits for which guess is plotted"""
 
-def get_lambda_error(guess, *args):
-    Da = args[0]
-    error = 1 - guess / Da * np.tan(guess)
-    return error
+    guess = np.arange(0, 30., 0.01)
 
-solution = get_lambda_error(guess, Da)
+    solution = cat_eigen.get_lambda_error(guess, Da)
 
-def solve_lambda(Da):
-    lambda_i = fsolve(
-        get_lambda_error, x0=cat_eigen.get_lambda(Da=Da), args=(Da)
-        )
-    return lambda_i
+    def solve_lambda(Da):
+        lambda_i = fsolve(
+            cat_eigen.get_lambda_error, x0=cat_eigen.get_lambda(Da=Da), args=(Da)
+            )
+        return lambda_i
 
-lambda_i = solve_lambda(Da)
-ydata = np.zeros(lambda_i.size)
+    lambda_i = solve_lambda(Da)
+    ydata = np.zeros(lambda_i.size)
 
-print 'lambda_i =', lambda_i
+    print 'lambda_i =', lambda_i
 
-# Plot configuration
-FONTSIZE = 15
-plt.rcParams['axes.labelsize'] = FONTSIZE
-plt.rcParams['axes.titlesize'] = FONTSIZE
-plt.rcParams['legend.fontsize'] = FONTSIZE
-plt.rcParams['xtick.labelsize'] = FONTSIZE
-plt.rcParams['ytick.labelsize'] = FONTSIZE
-plt.rcParams['lines.linewidth'] = 1.5
-plt.rcParams['lines.markersize'] = 10
+    # Plot configuration
+    FONTSIZE = 15
+    plt.rcParams['axes.labelsize'] = FONTSIZE
+    plt.rcParams['axes.titlesize'] = FONTSIZE
+    plt.rcParams['legend.fontsize'] = FONTSIZE
+    plt.rcParams['xtick.labelsize'] = FONTSIZE
+    plt.rcParams['ytick.labelsize'] = FONTSIZE
+    plt.rcParams['lines.linewidth'] = 1.5
+    plt.rcParams['lines.markersize'] = 10
 
-plt.close()
+    plt.close()
 
-fig1 = plt.figure()
-fig1.subplots_adjust(bottom=0.15)
-fig1.subplots_adjust(left=0.15)
-plt.plot(guess, solution, label='Da =' + str(Da))
-plt.plot(guess, np.absolute(solution), label='absolute')
-plt.plot(lambda_i, ydata, label='solver', marker='s', linestyle='',
-         color='black')
-plt.xlim(guess.min(), guess.max())
-plt.ylim(-20, 20)
-plt.grid()
-plt.xlabel(r'$\lambda$')
-plt.ylabel(r'$1 - \frac{\lambda_n}{Da} \tan \lambda_n$') 
-plt.title('Eigenvalues\nDa='+str(Da))
-# plt.legend(loc='best')
+    fig1 = plt.figure()
+    fig1.subplots_adjust(bottom=0.15)
+    fig1.subplots_adjust(left=0.15)
+    plt.plot(guess, solution, label='Da =' + str(Da))
+    plt.plot(guess, np.absolute(solution), label='absolute')
+    plt.plot(lambda_i, ydata, label='solver', marker='s', linestyle='',
+             color='black')
+    plt.xlim(guess.min(), guess.max())
+    plt.ylim(-500, 500)
+    plt.grid()
+    plt.xlabel(r'$\lambda$')
+    plt.ylabel(r'$1 - \frac{\lambda_n}{Da} \tan \lambda_n$') 
+    plt.title('Eigenvalues\nDa='+str(Da))
+    # plt.legend(loc='best')
+    
+    plt.show()
 
-plt.show()
-
-plt.savefig('../Plots/eigen.pdf')
-plt.savefig('../Plots/eigen.png')
+    plt.savefig('../Plots/eigen.pdf')
+    plt.savefig('../Plots/eigen.png')
 
