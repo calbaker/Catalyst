3773a2df62f6b431d006f242a1037debf50bb4af
Chad Baker <calbaker@utexas.edu>
2012-10-21 17:45:01 -0500

    now DeltaP is fixed

--- Instances/plot_eta_per_p.py
+++ Instances/plot_eta_per_p.py
@@ -5,6 +5,7 @@ import matplotlib.pyplot as plt
 import os
 import sys
 import numpy as np
+from scipy.optimize import fsolve
 
 cmd_folder = os.path.dirname('../Modules/')
 if cmd_folder not in sys.path:
@@ -15,71 +16,74 @@ reload(catalyst)
 
 cat_opt = catalyst.Catalyst()
 cat_opt.terms = 4
-cat_opt.Vdot = 50000.e-6 / 60. 
 cat_opt.T = 400. + 273.15
 cat_opt.set_TempPres_dependents(cat_opt.T)
 
-length = cat_opt.length
 height = cat_opt.height
-height_array = np.linspace(0.5, 10., 25) * 1.e-3
-length_array = (length * height) / height_array
+length = cat_opt.length
+Vdot = 500.e-6 / 60. 
 
-U = np.zeros(height_array.size)
-DeltaP_c_L = np.zeros(height_array.size)
-DeltaP_c_V = np.zeros(height_array.size)
+height_array = np.linspace(0.1, 5., 25) * height
+length_array = length * (height_array / height)
+Vdot_array = Vdot * height_array / height
 
-eta_c_L = np.zeros(height_array.size)
-eta_per_p_c_L = np.zeros(height_array.size)
-eta_c_V = np.zeros(height_array.size)
-eta_per_p_c_V = np.zeros(height_array.size)
+cat_opt.Vdot = Vdot
+
+DeltaP = np.zeros(height_array.size)
+eta = np.zeros(height_array.size)
+
+cat_opt.get_eta()
+U = cat_opt.U
+
+def get_DeltaP(length, *args):
+    height = args[0]
+    Re_h = U * height / cat_opt.air.nu
+    f = 24. / Re_h
+
+    perimeter = 2. * (height + cat_opt.width)
+    area = height * cat_opt.width
+    DeltaP = (
+        0.5 * f * perimeter * length / area * cat_opt.air.rho *
+        cat_opt.U ** 2.
+        )
+    return DeltaP
+
+DeltaP0 = get_DeltaP(length, height)
+
+def get_error(length, *args):
+    height = args[0]
+    DeltaP = get_DeltaP(length, height)
+    error = DeltaP - DeltaP0
+    return error
 
 for i in range(height_array.size):
-    # Constant Length
-    cat_opt.length = length
+    height = height_array[i]
+    length_array[i] = (
+        fsolve(get_error, x0=length_array[i], args=(height))
+        )
     cat_opt.height = height_array[i]
+    cat_opt.length = length_array[i]
+    cat_opt.Vdot = Vdot_array[i]
     cat_opt.x_ = cat_opt.length / cat_opt.height
 
     cat_opt.get_eta()
-    print "\n\n\nConstant Length"
-    print "Da =", cat_opt.Da
-    print "A_i =", cat_opt.A_i
-    print "lambda_i =", cat_opt.lambda_i
 
-    U[i] = cat_opt.U
-    Re_h = U[i] * height_array[i] / cat_opt.air.nu
+    U = cat_opt.U
+    Re_h = U * cat_opt.height / cat_opt.air.nu
     f = 24. / Re_h
 
     perimeter = 2. * (cat_opt.height + cat_opt.width)
     area = cat_opt.height * cat_opt.width
-
-    DeltaP_c_L[i] = (
+    DeltaP[i] = (
         0.5 * f * perimeter * cat_opt.length / area * cat_opt.air.rho
-        * cat_opt.U ** 2. * 1e-3
+        * cat_opt.U ** 2.
         )
+    eta[i] = cat_opt.eta
 
-    eta_c_L[i] = cat_opt.eta
-    eta_per_p_c_L[i] = eta_c_L[i] / DeltaP_c_L[i]
-
-    # Constant Volume
-    cat_opt.length = length_array[i]
-    cat_opt.x_ = cat_opt.length / cat_opt.height
-
-    cat_opt.get_eta()
-    print "\nConstant Volume"
-    print "Da =", cat_opt.Da
-    print "A_i =", cat_opt.A_i
-    print "lambda_i =", cat_opt.lambda_i
-
-    perimeter = 2. * (cat_opt.height + cat_opt.width)
-    area = cat_opt.height * cat_opt.width
-
-    DeltaP_c_V[i] = (
-        0.5 * f * perimeter * cat_opt.length / area * cat_opt.air.rho
-        * cat_opt.U ** 2. * 1e-3
-        )
-
-    eta_c_V[i] = cat_opt.eta
-    eta_per_p_c_V[i] = eta_c_V[i] / DeltaP_c_V[i]
+Wdot = DeltaP * Vdot_array * height / height_array
+eta_per_p = eta / DeltaP
+eta_per_Wdot = eta / Wdot
+eta_per_cat = eta * height_array / length_array
 
 # Plot configuration
 FONTSIZE = 18
@@ -93,54 +97,83 @@ plt.rcParams['lines.markersize'] = 8
 
 plt.close()
 
+# eta per p
 plt.figure('eta per p')
-plt.plot(height_array * 1e3, eta_per_p_c_L, label="Const. L")
-plt.plot(height_array * 1e3, eta_per_p_c_V, label="Const. V")
+plt.plot(height_array * 1e3, eta_per_p)
 plt.xlabel('Channel Height (mm)')
 plt.ylabel(
-    r'$\frac{\eta}{\Delta P}$ (%/kPa)'
+    r'$\frac{\eta}{\Delta P}$ (%/Pa)'
     )
 plt.grid()
-plt.legend(loc="best")
+# plt.legend(loc="best")
 plt.savefig(
     '../Plots/plot_eta_per_p/eta_per_p.pdf')
 paper_dir = '/home/chad/Documents/Catalyst/Paper/version 2.1/Figures/'
 plt.savefig(paper_dir + 'eta_per_p.pdf')
 
-plt.figure('eta per p v length')
-plt.plot(length_array * 1e3, eta_per_p_c_V)
-plt.xlabel('Channel Length (mm)')
+# eta per cat
+plt.figure('eta per cat')
+plt.plot(height_array * 1e3, eta_per_cat)
+plt.xlabel('Channel Height (mm)')
 plt.ylabel(
-    r'$\frac{\eta}{\Delta P}$ (%/kPa)'
+    r'$\frac{\eta}{A}$ (%/m$^2$)'
     )
 plt.grid()
+# plt.legend(loc="best")
 plt.savefig(
-    '../Plots/plot_eta_per_p/eta_per_p_v_length.pdf')
+    '../Plots/plot_eta_per_p/eta_per_cat.pdf')
 paper_dir = '/home/chad/Documents/Catalyst/Paper/version 2.1/Figures/'
-plt.savefig(paper_dir + 'eta_per_p_v_length.pdf')
+plt.savefig(paper_dir + 'eta_per_cat.pdf')
 
+# eta per w
+plt.figure('eta per w')
+plt.plot(height_array * 1e3, eta_per_Wdot)
+plt.xlabel('Channel Height (mm)')
+plt.ylabel(
+    r'$\frac{\eta}{\dot{W}}$ (%/W)'
+    )
+plt.grid()
+# plt.legend(loc="best")
+plt.subplots_adjust(left=0.17)
+plt.savefig(
+    '../Plots/plot_eta_per_p/eta_per_Wdot.pdf')
+paper_dir = '/home/chad/Documents/Catalyst/Paper/version 2.1/Figures/'
+plt.savefig(paper_dir + 'eta_per_Wdot.pdf')
+
+# 'eta'
 plt.figure('eta')
-plt.plot(height_array * 1e3, eta_c_L, label="Const. L")
-plt.plot(height_array * 1e3, eta_c_V, label="Const. V")
+plt.plot(height_array * 1e3, eta)
 plt.xlabel('Channel Height (mm)')
 plt.ylabel('Conversion Efficiency (%)')
 plt.grid()
-plt.legend(loc="best")
+# plt.legend(loc="best")
 plt.savefig(
     '../Plots/plot_eta_per_p/eta.pdf')
 paper_dir = '/home/chad/Documents/Catalyst/Paper/version 2.1/Figures/'
 plt.savefig(paper_dir + 'eta.pdf')
 
+# delta p
 plt.figure('delta p')
-plt.plot(height_array * 1e3, DeltaP_c_L, label="Const. L")
-plt.plot(height_array * 1e3, DeltaP_c_V, label="Const. V")
+plt.plot(height_array * 1e3, DeltaP)
 plt.xlabel('Channel Height (mm)')
-plt.ylabel(r'$\Delta P$ (kPa)')
+plt.ylabel(r'$\Delta P$ (Pa)')
 plt.grid()
-plt.legend(loc="best")
+# plt.legend(loc="best")
 plt.savefig(
     '../Plots/plot_eta_per_p/deltaP.pdf')
 paper_dir = '/home/chad/Documents/Catalyst/Paper/version 2.1/Figures/'
 plt.savefig(paper_dir + 'deltaP.pdf')
+
+# Wdot
+plt.figure('Wdot')
+plt.plot(height_array * 1e3, Wdot * 1e3)
+plt.xlabel('Channel Height (mm)')
+plt.ylabel(r'$\dot{W}$ ($\mu$W)')
+plt.grid()
+# plt.legend(loc="best")
+plt.savefig(
+    '../Plots/plot_eta_per_p/Wdot.pdf')
+paper_dir = '/home/chad/Documents/Catalyst/Paper/version 2.1/Figures/'
+plt.savefig(paper_dir + 'Wdot.pdf')
 
 plt.show()
